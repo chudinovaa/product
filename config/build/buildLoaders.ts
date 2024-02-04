743c2edbb52b653b619a4d6a9ee487c7cf14c921
Anton <Nutsway@e1.ru>
2024-02-05 00:18:14 +0100

    add babel, add extract plugin, add bundle analyzer plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,41 @@ import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
-export function buildLoaders({isDev} : BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
+    const babelLoader = {
+
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {locales: ['ru', 'en'],
+                        keyAsDefaultValue: true}
+                    ],
+                ]
+            }
+        }
+    }
+
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
@@ -31,6 +65,9 @@ export function buildLoaders({isDev} : BuildOptions): webpack.RuleSetRule[] {
     }
 
     return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader
     ]
---
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useTranslation} from "react-i18next";
+
+const AboutPage = () => {
+    const {t} = useTranslation()
+    return (
+        <div>
+            {t('О сайте')}
+            {t('О нас')}
+        </div>
+    );
+};
+export default AboutPage
